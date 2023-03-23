8da77098b470290751d5064d332d352ed6c4d5c7
Niels <niels.vandeginste@student.ap.be>
2023-03-23 16:48:31 +0100

    menu logica implementeren

--- serverOK.py
+++ serverOK.py
@@ -1,24 +1,77 @@
 from ping3 import verbose_ping
+import traceback
+import os
+import time
 checkMode=""
 
 def mainLoop():
+    os.system('cls||clear')
     print("###### Welkom bij de checkonator ######")
-    print("Kies een van de onderstaande keuzes:")
-    print("1. Voeg een host toe aan de targets. ")
-    print("2. Geef de actieve hosts op.")
-    print("3. Start een check uit op de targets.")
+    print("Kies een van de onderstaande opties:")
+    print("1. Target hosts beheren.")
+    print("2. Checkmodus beheren.")
+    print("q. Het project afsluiten.")
     print("")
-
-    keuze=int(input("Geef keuze op: "))
+    keuze=input("Geef keuze op: ")
 
     match keuze:
-        case 3:
-            chooseCheckMode()
+        case "1":
+            manageHosts()
+        case "2":
+            manageCheckMode()
+        case "q":
+            quit()
+        case _:
+            print("Optie niet gevonden")
+            time.sleep(0.5)
+            mainLoop()
 
-def chooseCheckMode():
-    print("Welk type check wilt u uitvoeren ?")
-    print("- ping. voer een ping check uit.")
-    checkMode=input("Geef keuze op: ")
+def manageHosts():
+    os.system('cls||clear')
+    print("###### Beheer van hosts ######")
+    print("Kies een van de onderstaande opties:")
+    print("1. Host toevoegen.")
+    print("2. Host verwijderen.")
+    print("3. Hosts weergeven.")
+    print("q. terug naar hoofdmenu")
+    print("")
+    keuze=input("Geef keuze op: ")
+
+    match keuze:
+        case "1":
+            manageHosts()
+        case "q":
+            mainLoop()
+        case _:
+            print("Optie niet gevonden")
+            time.sleep(0.5)
+            manageHosts()
+
+def manageCheckMode():
+    os.system('cls||clear')
+    print("###### Beheer van checkmodus ######")
+    print("Welke check wilt u activeren ?: ")
+    print("1. ping")
+    print("2. ping.")
+    print("3. ping <-- beste keuze :)")
+    print("q. terug naar hoofdmenu")
+    print("")
+    keuze=input("Geef keuze op: ")
+    match keuze:
+        case "1":
+            checkMode="ping"
+        case "2":
+            checkMode="ping"
+        case "3":
+            checkMode="ping"
+        case "q":
+            mainLoop()
+        case _:
+            print("Optie niet gevonden")
+            time.sleep(0.5)
+            manageCheckMode()
+    mainLoop()
+
 
 
 if __name__ == "__main__":
